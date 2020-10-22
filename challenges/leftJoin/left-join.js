a321c602aa30a85fe6afa4ff84d0ba470f2fdc33
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-10-23 00:37:00 +0300

    done left join

--- challenges/leftJoin/left-join.js
+++ challenges/leftJoin/left-join.js
@@ -1,41 +1,12 @@
 
-function leftJoin(hashtable1, hashtable2) {
-
-  const resultArr = [];
-
-  hashtable1.buckets.forEach(outerBucket => {
-
-    outerBucket.forEach(innerBucket => {
-
-      if (innerBucket) {
-        resultArr.push([innerBucket[0], innerBucket[1]]);
-      } else {
-        return;
+function leftJoin(hashA, hashB) {
+  hashA.entries.forEach((value, index) => {
+    if(value) {
+      if (hashB.entries[index]) {
+        hashA.entries[index].appendLL(hashB.entries[index])
       }
-    });
-  });
-
-  for (let i = 0; i < resultArr.length; i++) {
-
-    let keyToCheck = resultArr[i][0];
-
-    if (hashtable2.get(keyToCheck)) {
-
-      const secondValToAddOntoResultsArr = hashtable2.get(keyToCheck);
-
-      resultArr[i].push(secondValToAddOntoResultsArr);
-
-    } else {
-
-      resultArr[i].push(null);
-
     }
-
-  }
-
-  return resultArr;
-
-
+  });
 }
 
 module.exports = leftJoin;
